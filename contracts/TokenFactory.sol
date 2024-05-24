1444b765029d0a554786587322d714c6fc22e52c
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-05-24 09:58:41 -0300

    token factory updated

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -18,7 +18,7 @@ contract TokenFactory is AccessControl, Ownable {
         address rwa;
         address insurance;
     }
-    event TokenRWADeployed(address indexed tokenAddress, uint256 indexed projectIndex, string name, string symbol, uint256 totalSupply, uint dueDate, uint yield);
+    event TokenRWADeployed(address indexed tokenAddress, uint256 indexed projectIndex, string name, string symbol, uint256 totalSupply, uint256 totalValue, uint dueDate, uint yield);
     event TokenInsuranceDeployed(address indexed tokenAddress, uint256 indexed projectIndex, string name, string symbol, uint256 totalSupply, uint dueDate, uint yield, uint prime);
 
     constructor() Ownable(msg.sender) {
@@ -26,16 +26,19 @@ contract TokenFactory is AccessControl, Ownable {
         _grantRole(ADMIN_ROLE, msg.sender);
     }
 
-    function createSecuredRWA(string calldata name_, string calldata symbol_, uint256 totalSupply_, uint dueDate_, uint yield_, uint prime_) public onlyRole(ADMIN_ROLE) returns (address, address) {
-        TokenRWA rwa = new TokenRWA(name_, symbol_, totalSupply_, dueDate_, yield_);
-        TokenInsurance insurance = new TokenInsurance(name_, symbol_, totalSupply_, dueDate_, yield_, address(rwa), prime_);
+    function createSecuredRWA(string calldata name_, string calldata symbol_, uint256 totalSupply_, uint256 totalValue_, uint dueDate_, uint yield_, uint prime_, address vault_) public onlyRole(ADMIN_ROLE) returns (address, address) {
+        TokenRWA rwa = new TokenRWA(name_, symbol_, totalSupply_, totalValue_, dueDate_, yield_);
+        TokenInsurance insurance = new TokenInsurance(name_, symbol_, dueDate_, yield_, address(rwa), prime_, vault_);
         SecuredRwa memory item = SecuredRwa({ rwa: address(rwa), insurance: address(insurance) });
         tokens.push(item);
         rwaSecured[address(rwa)] = address(insurance);
         tokenCount += 1;
-        emit TokenRWADeployed(address(rwa), tokenCount, name_, symbol_, totalSupply_, dueDate_, yield_);
+        emit TokenRWADeployed(address(rwa), tokenCount, name_, symbol_, totalSupply_, totalValue_, dueDate_, yield_);
         emit TokenInsuranceDeployed(address(insurance), tokenCount, name_, symbol_, totalSupply_, dueDate_, yield_, prime_);
         return (address(rwa), address(insurance));
     }
 
+    function getAllTokens() external view returns(SecuredRwa[] memory) {
+        return tokens;
+    }
 }
