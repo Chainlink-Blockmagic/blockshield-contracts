98722b27a28fb372bc025e2e0a2f502c3957a3a6
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-05-24 10:00:15 -0300

    interfaces added

---
+++ contracts/interfaces/IVault.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IVault {
+    function addInsurance(address securedAsset, address insuranceClient, uint256 quantity, uint256 securedAmount) external payable returns (bool);
+    function handleRWAPayment(bool liquidationResponse, address securedAsset_) external returns (bool);
+}
