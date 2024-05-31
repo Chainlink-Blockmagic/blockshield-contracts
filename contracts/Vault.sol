2bf285c4f870228614755912811efaf0d233d752
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-05-30 22:47:45 -0300

    vault refactor

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/access/AccessControl.sol";
+// TODO: It brings conflict with supportInterface of CCIP contract inherited by BlockshieldMessageReceiver.sol
+// import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/ITokenInsurance.sol";
 import "./interfaces/ITokenRWA.sol";
+import "./BlockshieldMessageSender.sol";
+import "./VaultMessageReceiver.sol";
 
 contract Vault is
-    AccessControl,
-    Ownable {
+    // AccessControl,
+    Ownable,
+    BlockshieldMessageSender,
+    VaultMessageReceiver {
 
-    /// @dev Access control constants
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    using SafeERC20 for IERC20;
+
+    // /// @dev Access control constants
+    // bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     /// @notice Maps for each secured RWA an owner with insurance details
     /// @dev This structure only allows to have one insurance per client per RWA
@@ -33,12 +41,20 @@ contract Vault is
 
     event InsurancePaid (address indexed securedAsset_, address indexed insuranceClient_, uint256 quantity_, uint256 securedAmount_, uint256 insuranceCost);
     event RWAYieldPaid (address indexed securedAsset_, address indexed insuranceClient_, uint256 quantity_, uint256 securedAmount_, uint256 insuranceCost);
+    event InsuranceHiringAdded(address indexed securedAsset_, address indexed insuranceClient_, uint256 amount);
+    event InsuranceTotalPayment(address indexed securedAsset_, address paymentReceiver, uint256 quantity_);
 
-    constructor() Ownable(msg.sender) {
-        _grantRole(ADMIN_ROLE, msg.sender);
+    constructor(
+        address routerCCIP_
+    )
+        Ownable(msg.sender)
+        BlockshieldMessageSender(routerCCIP_)
+        VaultMessageReceiver(routerCCIP_)
+    {
+        // _grantRole(ADMIN_ROLE, msg.sender);
     }
 
-    function addHiredInsurance(address securedAsset_, address insuranceClient_, uint256 quantity_, uint256 securedAmount_) external payable onlyRole(ADMIN_ROLE) {
+    function addHiredInsurance(address securedAsset_, address insuranceClient_, uint256 quantity_, uint256 securedAmount_) external {
         // CHECK
         require(securedAsset_ != address(0), "securedAsset_ cannot be zero address");
         require(insuranceClient_ != address(0), "insuranceClient_ cannot be zero address");
@@ -54,81 +70,95 @@ contract Vault is
         }
         existsInsuranceClient[insuranceClient_] = true;
         amountByAsset[securedAsset_] += securedAmount_;
+
+        // TODO: Refactor to send CCIP message
+        // Allow TokenInsurance to spend TokenRWA balance in its behalf
+        ITokenRWA(securedAsset_).allowSpendTokens(address(this), quantity_);
+
+        // Transfer tokens from TokenRWA to Vault
+        IERC20(securedAsset_).safeTransferFrom(securedAsset_, address(this), quantity_);
+
+        emit InsuranceHiringAdded(securedAsset_, insuranceClient_, securedAmount_);
     }
 
-    function handleRWAPayment(bool liquidationResponse, address insurance) external {
-        if (!liquidationResponse) payInsurance(insurance);
-        else payRWAWithYield(insurance);
+    function handleRWAPayment(bool liquidationResponse, address securedAsset, uint256 insurancePrime) external {
+        if (!liquidationResponse) payInsurance(securedAsset, insurancePrime);
+        else payRWAWithYield(securedAsset, insurancePrime);
     }
 
-    function payInsurance(address insurance) internal {
-        payUser(insurance, false);
+    function payInsurance(address securedAsset, uint256 insurancePrime) internal {
+        payUser(securedAsset, false, insurancePrime);
     }
 
-    function payRWAWithYield(address insurance) internal {
-        payUser(insurance, true);
+    function payRWAWithYield(address securedAsset, uint256 insurancePrime) internal {
+        payUser(securedAsset, true, insurancePrime);
     }
 
-    function payUser(address insurance, bool isInsuracePaid) internal {
-        require(insurance != address(0), "insurance cannot be zero address");
+    function payUser(address securedAsset, bool isInsuracePaid, uint256 insurancePrime) internal {
+        require(securedAsset != address(0), "insurance cannot be zero address");
 
-        ITokenInsurance insuranceContract = ITokenInsurance(insurance);
-        address securedAsset = insuranceContract.securedAsset();
         address[] memory insuranceOwners = insuranceOwnersByAsset[securedAsset];
+
+        uint256 totalAmount;
 
         for (uint i = 0; i < insuranceOwners.length; i++) {
             address currentInsuranceOwner = insuranceOwners[i];
 
             InsuranceDetails memory insuranceDetails = hiredInsurances[securedAsset][currentInsuranceOwner];
-            uint256 insuranceCost = getInsuranceCost(insuranceContract, insuranceDetails.quantity);
+            uint256 insuranceCost = getInsuranceCost(securedAsset, insurancePrime, insuranceDetails.quantity);
 
             uint256 amountToTransfer;
             if (isInsuracePaid) amountToTransfer = insuranceDetails.securedAmount - insuranceCost;
             else amountToTransfer = (insuranceDetails.quantity * ITokenRWA(securedAsset).calculateRWAValuePlusYield()) - insuranceCost;
 
-            payable(currentInsuranceOwner).transfer(amountToTransfer);
+            // payable(currentInsuranceOwner).transfer(amountToTransfer);
+
+            totalAmount += amountToTransfer;
 
             if (isInsuracePaid) emit InsurancePaid(securedAsset, currentInsuranceOwner, insuranceDetails.quantity, insuranceDetails.securedAmount, insuranceCost);
             else emit RWAYieldPaid(securedAsset, currentInsuranceOwner, insuranceDetails.quantity, insuranceDetails.securedAmount, insuranceCost);
         }
 
+        sendMethodCallWithUSDC(totalAmount, abi.encode(""));
+
         // TODO: SEND CCIP CALL TO TOKEN INSURANCE PRA TokenInsurance#selfdestruct()
+
+        emit InsuranceTotalPayment(securedAsset, destinationReceiver, totalAmount);
     }
 
-    function getInsuranceCost(ITokenInsurance insuranceContract, uint256 quantity) internal returns (uint256 insuranceCost) {
-        uint256 prime = insuranceContract.prime();
-        address securedAsset = insuranceContract.securedAsset();
+    function getInsuranceCost(address securedAsset, uint256 insurancePrime, uint256 quantity) internal view returns (uint256 insuranceCost) {
         uint256 rwaValue = ITokenRWA(securedAsset).unitValue();
         uint256 rwaDecimals = ITokenRWA(securedAsset).decimals();
-        uint256 insuranceValue = rwaValue * prime / 10 ** rwaDecimals;
+        uint256 insuranceValue = rwaValue * insurancePrime / 10 ** rwaDecimals;
         insuranceCost = quantity * insuranceValue / 10 ** rwaDecimals;
     }
 
-    function withdraw() external onlyRole(ADMIN_ROLE) {
+    function withdraw() external onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
 
-    /// @notice Grants the ADMIN_ROLE to an account.
-    /// @dev Throw if msg.sender has not ADMIN_ROLE role.
-    /// @dev Throw if account is address zero. Message: "TokenInsurance: account is the zero address"
-    /// @param account The address to grant the role
-    function grantAdminRole(address account) public onlyRole(ADMIN_ROLE) {
-        require(account != address(0), "Vault: account is the zero address");
-        _grantRole(ADMIN_ROLE, account);
-    }
+    // /// @notice Grants the ADMIN_ROLE to an account.
+    // /// @dev Throw if msg.sender has not ADMIN_ROLE role.
+    // /// @dev Throw if account is address zero. Message: "TokenInsurance: account is the zero address"
+    // /// @param account The address to grant the role
+    // function grantAdminRole(address account) public onlyRole(ADMIN_ROLE) {
+    //     require(account != address(0), "Vault: account is the zero address");
+    //     _grantRole(ADMIN_ROLE, account);
+    // }
 
-    /// @notice Revokes the ADMIN_ROLE from an account.
-    /// @dev Throw if msg.sender has not ADMIN_ROLE role.
-    /// @dev Throw if account is address zero. Message: "TokenInsurance: Cannot revoke own admin role"
-    /// @param account The address to grant the role
-    function revokeAdminRole(address account) public onlyRole(ADMIN_ROLE) {
-        require(account != msg.sender, "Vault: Cannot revoke own admin role"); // Prevent self-revocation
-        _revokeRole(ADMIN_ROLE, account);
-    }
-
-    // Function to receive Ether. msg.data must be empty
-    receive() external payable {}
+    // /// @notice Revokes the ADMIN_ROLE from an account.
+    // /// @dev Throw if msg.sender has not ADMIN_ROLE role.
+    // /// @dev Throw if account is address zero. Message: "TokenInsurance: Cannot revoke own admin role"
+    // /// @param account The address to grant the role
+    // function revokeAdminRole(address account) public onlyRole(ADMIN_ROLE) {
+    //     require(account != msg.sender, "Vault: Cannot revoke own admin role"); // Prevent self-revocation
+    //     _revokeRole(ADMIN_ROLE, account);
+    // }
 
     // Fallback function is called when msg.data is not empty
     fallback() external payable {}
+
+    function approve(address _transferTokenAddress, address _router, uint256 _amount) internal override {
+        IERC20(_transferTokenAddress).approve(address(_router), _amount);
+    }
 }
