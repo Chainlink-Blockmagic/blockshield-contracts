4ccbca112c9cc70139fa004d8ba17f17f58f5298
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-05-31 13:41:08 -0300

    TokenInsurance client payment implmentation with CCIP calls

--- contracts/TokenInsurance.sol
+++ contracts/TokenInsurance.sol
@@ -7,34 +7,34 @@ import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/Fu
 import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
 
-import "@openzeppelin/contracts/access/AccessControl.sol";
+// TODO: It brings conflict with supportInterface of CCIP contract inherited by BlockshieldMessageReceiver.sol
+// import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "./libraries/PercentageUtils.sol";
 import "./FunctionWithUpdateRequest.sol";
 import "./BlockshieldMessageSender.sol";
-
-import "hardhat/console.sol";
+import "./BlockshieldMessageReceiver.sol";
 
 contract TokenInsurance is
     ERC20,
     ERC20Burnable,
     ReentrancyGuard,
     AutomationCompatibleInterface,
-    AccessControl,
+    // AccessControl,
     FunctionWithUpdateRequest,
-    BlockshieldMessageSender
+    BlockshieldMessageSender,
+    BlockshieldMessageReceiver
 {
     using SafeERC20 for IERC20;
     using PercentageUtils for uint256;
     // using FunctionsRequest for FunctionsRequest.Request;
 
-   /// @dev Access control constants
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+//    /// @dev Access control constants
+//     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     /// @notice Vault is the contract who holds the RWAs tokens and waits liquidation
     address public vault;
@@ -48,6 +48,9 @@ contract TokenInsurance is
     /// @notice Automation
     bool public alreadyExecuted;
 
+    /// @notice Clients who bought insurance
+    address[] insuranceClients;
+
     TokenRWAInfo public tokenRWAInfo;
     struct TokenRWAInfo {
         uint256 totalSupply;
@@ -55,9 +58,12 @@ contract TokenInsurance is
         uint256 decimals;
         uint256 dueDate;
         string symbol;
+        bool isSet;
     }
 
-    event InsuranceHired(address indexed sender, address insurance, uint256 amount);
+    event InsuranceHired(bytes32 indexed messageId, address indexed sender, address insurance, uint256 amount);
+    event HandlePayment(bytes32 indexed messageId, address indexed securedAsset, bool liquidationResponse);
+    event UserPayment(address indexed inusrance, address indexed client, uint256 paymentValue, uint256 totalValue, uint256 insuranceTotalCost);
 
     /// @notice It will mint the total supply of the RWA secured asset to the contract itself
     constructor(
@@ -66,58 +72,59 @@ contract TokenInsurance is
         address securedAsset_,
         address vault_,
         uint256 prime_,
-        uint256 yield_,
         address routerFunctions_,
         address routerCCIP_
     )
         ERC20(name_, symbol_)
         FunctionWithUpdateRequest(routerFunctions_, msg.sender)
         BlockshieldMessageSender(routerCCIP_)
+        BlockshieldMessageReceiver(routerCCIP_)
     {
         require(bytes(name_).length > 0, "Name cannot be empty");
         require(bytes(symbol_).length > 0, "Symbol cannot be empty");
-        require(bytes(symbol_).length > 3, "Symbol must be longer than 3 characters");
-        require(securedAsset_ != address(0), "Secured asset cannot be zero");
-        require(vault_ != address(0), "Vault address cannot be zero");
+        require(bytes(symbol_).length > 3, "Symbol min length is 3");
+        if (securedAsset_ == address(0)) revert ZeroAddress();
+        if (vault_ == address(0)) revert ZeroAddress();
         require(prime_.checkPercentageThreshold(), "Invalid prime percentage");
-        require(yield_.checkPercentageThreshold(), "Prime must be less than yield");
-        require(prime_ < yield_, "Prime must be less than yield");
 
         vault = vault_;
         securedAsset = securedAsset_;
         prime = prime_;
 
-        _grantRole(ADMIN_ROLE, msg.sender);
-        _grantRole(ADMIN_ROLE, address(this));
+        // _grantRole(ADMIN_ROLE, msg.sender);
+        // _grantRole(ADMIN_ROLE, address(this));
     }
 
     function hireInsurance(uint256 quantity_) external nonReentrant {
-        require(quantity_ > 0, "Cannot secure zero tokens");
-        require(transferTokenAddress != address(0), "transferTokenAddress cannot be zero address");
-        require(quantity_ <= tokenRWAInfo.totalSupply, "Cannot secure more than associated RWA supply");
-        require(totalSupply() + quantity_ <= tokenRWAInfo.totalSupply, "Cannot secure desired amount of tokens");
+        if (transferTokenAddress == address(0)) revert ZeroAddress();
+        require(tokenRWAInfo.isSet, "tokenRWAInfo is not set yet");
+        require(quantity_ > 0, "Invalid quantity");
+        require(quantity_ <= tokenRWAInfo.totalSupply, "Quantity is greater than supply");
+        require(totalSupply() + quantity_ <= tokenRWAInfo.totalSupply, "Too high quantity");
 
         // Calculate the required amount for insurance payment
-        uint256 requiredAmount_ = tokenRWAInfo.unitValue * quantity_ / 10 ** tokenRWAInfo.decimals;
+        uint256 requiredAmount_ = getRwaTotalValue(quantity_);
 
         // Validate USDC amount
-        require(IERC20(transferTokenAddress).balanceOf(msg.sender) == requiredAmount_, "Insufficient USDC to hire insurance");
+        require(IERC20(transferTokenAddress).balanceOf(msg.sender) == requiredAmount_, "Insufficient USDC");
         // TODO: check for possible maximum amount per user
-
-        // Mint TokenInsurance desired amount of tokens to msg.sender
-        _mint(msg.sender, quantity_);
 
         // Make CCIP call sending USDC from msg.sender and making a contract call
         bytes memory data = abi.encodeWithSignature(
-            "addHiredInsurance(address,address,uint256,uint256)",
+            "addHiredInsurance(address,address,address,uint256,uint256)",
             securedAsset,
+            address(this),
             msg.sender,
             quantity_,
             requiredAmount_
         );
-        sendMethodCallWithUSDC(requiredAmount_, data);
+        bytes32 messageId = sendMethodCallWithUSDC(vault, requiredAmount_, data);
 
-        emit InsuranceHired(msg.sender, address(this), requiredAmount_);
+        // Mint TokenInsurance desired amount of tokens to msg.sender
+        _mint(msg.sender, quantity_);
+        insuranceClients.push(msg.sender);
+
+        emit InsuranceHired(messageId, msg.sender, address(this), requiredAmount_);
     }
 
     function isDueDateArrived() internal view returns (bool) {
@@ -131,13 +138,12 @@ contract TokenInsurance is
 
     function performUpkeep(bytes calldata /* performData */) external override {
         (bool upkeepNeeded, ) = this.checkUpkeep(abi.encode(""));
-        require(upkeepNeeded, "RWA asset was not yet liquidated or already executed");
+        require(upkeepNeeded, "checkUpkeep not met");
         sendGetLiquidationRequest(securedAsset, tokenRWAInfo.symbol);
         alreadyExecuted = true;
     }
 
     function callVaultHandleRWAPayment() internal override {
-        // IVault(vault).handleRWAPayment(liquidationResponse, securedAsset);
         bool liquidationResponse = s_settled;
         bytes memory data = abi.encodeWithSignature(
             "handleRWAPayment(bool,address,uint256)",
@@ -145,14 +151,52 @@ contract TokenInsurance is
             securedAsset,
             prime
         );
-        sendMethodCallWithUSDC(0, data);
+        bytes32 messageId = sendMethodCallWithUSDC(vault, 0, data);
+        emit HandlePayment(messageId, securedAsset, liquidationResponse);
+    }
+
+    function updateTokenRWADetails(TokenRWAInfo calldata rwa) external onlyOwner {
+        tokenRWAInfo = rwa;
+    }
+
+    function payInsuranceClients() external {
+        for (uint i = 0; i < insuranceClients.length; i++) {
+            address currentInsuranceOwner = insuranceClients[i];
+            uint256 quantity = IERC20(address(this)).balanceOf(currentInsuranceOwner);
+            (uint256 totalValue, uint256 insuranceTotalCost) = getUserPaymentAmount(quantity);
+            uint256 paymentValue = totalValue - insuranceTotalCost;
+            IERC20(transferTokenAddress).safeTransfer(currentInsuranceOwner, paymentValue);
+            _burn(currentInsuranceOwner, quantity);
+            emit UserPayment(address(this), currentInsuranceOwner, paymentValue, totalValue, insuranceTotalCost);
+        }
+    }
+
+    function getUserPaymentAmount(uint256 quantity) internal view returns (uint256, uint256) {
+        uint256 rwaUnitValue = tokenRWAInfo.unitValue;
+        uint256 rwaDecimals = tokenRWAInfo.decimals;
+        uint256 insuranceUnitValue = rwaUnitValue * prime / 10 ** rwaDecimals;
+        uint256 insuranceTotalCost = quantity * insuranceUnitValue / 10 ** rwaDecimals;
+        uint256 totalValue = getRwaTotalValue(quantity);
+        return (totalValue, insuranceTotalCost);
+    }
+
+    function getRwaTotalValue(uint256 quantity) internal view returns (uint256 totalValue) {
+        totalValue = tokenRWAInfo.unitValue * quantity / 10 ** tokenRWAInfo.decimals;
+    }
+
+    function setVault(address vault_) external onlyOwner {
+        if (vault_ != address(0)) {
+            vault = vault_;
+        }
+    }
+
+    function setSecuredAsset(address securedAsset_) external onlyOwner {
+        if (securedAsset_ != address(0)) {
+            securedAsset = securedAsset_;
+        }
     }
 
     function approve(address _transferTokenAddress, address _router, uint256 _amount) internal override {
         IERC20(_transferTokenAddress).approve(address(_router), _amount);
-    }
-
-    function updateTokenRWADetails(TokenRWAInfo calldata rwa) external onlyRole(ADMIN_ROLE) {
-        tokenRWAInfo = rwa;
     }
 }
