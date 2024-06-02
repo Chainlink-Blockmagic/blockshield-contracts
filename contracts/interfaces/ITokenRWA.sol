114d81fd78a38c0938633a3d4abadbca64f57659
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-06-01 23:34:00 -0300

    fix add insurance method

--- contracts/interfaces/ITokenRWA.sol
+++ contracts/interfaces/ITokenRWA.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.24;
 
 interface ITokenRWA {
     function symbol() external view returns (string memory);
-    function decimals() external view returns (uint256);
+    function decimals() external view returns (uint8);
     function yield() external view returns (uint256);
     function dueDate() external view returns (uint256);
-    function unitValue() external view returns (uint256);
+    function getRwaUnitValueInTokenTransferDecimals() external view returns (uint256);
+    function getPaymentTokenDecimals() external view returns (uint8 unitValue);
     function calculateRWAValuePlusYield() external view returns (uint256);
     function allowSpendTokens(address spender, uint256 value) external;
 }
